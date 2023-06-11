8d0cb6e0d8ac88baab0a64e0700a9dd4f6bf9abe
Fazan4ik <jagannath2006@outlook.com>
2023-06-12 00:27:42 +0300

    Добавьте файлы проекта.

---
+++ HWFormsmaze/Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace HWFormsmaze
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            poisonHP2 = new Label();
+            poisonHP1 = new Label();
+            labelCoin7 = new Label();
+            labelCoin6 = new Label();
+            labelCoin5 = new Label();
+            labelCoin4 = new Label();
+            labelCoin3 = new Label();
+            labelCoin2 = new Label();
+            labelCoin1 = new Label();
+            labelCoinText = new Label();
+            labelHP = new Label();
+            label13 = new Label();
+            label12 = new Label();
+            label11 = new Label();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            hero = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(poisonHP2);
+            panel1.Controls.Add(poisonHP1);
+            panel1.Controls.Add(labelCoin7);
+            panel1.Controls.Add(labelCoin6);
+            panel1.Controls.Add(labelCoin5);
+            panel1.Controls.Add(labelCoin4);
+            panel1.Controls.Add(labelCoin3);
+            panel1.Controls.Add(labelCoin2);
+            panel1.Controls.Add(labelCoin1);
+            panel1.Controls.Add(labelCoinText);
+            panel1.Controls.Add(labelHP);
+            panel1.Controls.Add(label13);
+            panel1.Controls.Add(label12);
+            panel1.Controls.Add(label11);
+            panel1.Controls.Add(label10);
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(hero);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(801, 450);
+            panel1.TabIndex = 0;
+            // 
+            // poisonHP2
+            // 
+            poisonHP2.AutoSize = true;
+            poisonHP2.BackColor = Color.Green;
+            poisonHP2.Location = new Point(321, 141);
+            poisonHP2.Name = "poisonHP2";
+            poisonHP2.Size = new Size(29, 15);
+            poisonHP2.TabIndex = 29;
+            poisonHP2.Text = "heal";
+            // 
+            // poisonHP1
+            // 
+            poisonHP1.AutoSize = true;
+            poisonHP1.BackColor = Color.Green;
+            poisonHP1.Location = new Point(38, 360);
+            poisonHP1.Name = "poisonHP1";
+            poisonHP1.Size = new Size(29, 15);
+            poisonHP1.TabIndex = 28;
+            poisonHP1.Text = "heal";
+            // 
+            // labelCoin7
+            // 
+            labelCoin7.AutoSize = true;
+            labelCoin7.BackColor = Color.Yellow;
+            labelCoin7.Location = new Point(621, 161);
+            labelCoin7.Name = "labelCoin7";
+            labelCoin7.Size = new Size(32, 15);
+            labelCoin7.TabIndex = 27;
+            labelCoin7.Text = "Coin";
+            // 
+            // labelCoin6
+            // 
+            labelCoin6.AutoSize = true;
+            labelCoin6.BackColor = Color.Yellow;
+            labelCoin6.Location = new Point(636, 360);
+            labelCoin6.Name = "labelCoin6";
+            labelCoin6.Size = new Size(32, 15);
+            labelCoin6.TabIndex = 26;
+            labelCoin6.Text = "Coin";
+            // 
+            // labelCoin5
+            // 
+            labelCoin5.AutoSize = true;
+            labelCoin5.BackColor = Color.Yellow;
+            labelCoin5.Location = new Point(360, 83);
+            labelCoin5.Name = "labelCoin5";
+            labelCoin5.Size = new Size(32, 15);
+            labelCoin5.TabIndex = 25;
+            labelCoin5.Text = "Coin";
+            // 
+            // labelCoin4
+            // 
+            labelCoin4.AutoSize = true;
+            labelCoin4.BackColor = Color.Yellow;
+            labelCoin4.Location = new Point(360, 276);
+            labelCoin4.Name = "labelCoin4";
+            labelCoin4.Size = new Size(32, 15);
+            labelCoin4.TabIndex = 24;
+            labelCoin4.Text = "Coin";
+            // 
+            // labelCoin3
+            // 
+            labelCoin3.AutoSize = true;
+            labelCoin3.BackColor = Color.Yellow;
+            labelCoin3.Location = new Point(226, 227);
+            labelCoin3.Name = "labelCoin3";
+            labelCoin3.Size = new Size(32, 15);
+            labelCoin3.TabIndex = 23;
+            labelCoin3.Text = "Coin";
+            // 
+            // labelCoin2
+            // 
+            labelCoin2.AutoSize = true;
+            labelCoin2.BackColor = Color.Yellow;
+            labelCoin2.Location = new Point(125, 390);
+            labelCoin2.Name = "labelCoin2";
+            labelCoin2.Size = new Size(32, 15);
+            labelCoin2.TabIndex = 22;
+            labelCoin2.Text = "Coin";
+            // 
+            // labelCoin1
+            // 
+            labelCoin1.AutoSize = true;
+            labelCoin1.BackColor = Color.Yellow;
+            labelCoin1.Location = new Point(75, 288);
+            labelCoin1.Name = "labelCoin1";
+            labelCoin1.Size = new Size(32, 15);
+            labelCoin1.TabIndex = 21;
+            labelCoin1.Text = "Coin";
+            // 
+            // labelCoinText
+            // 
+            labelCoinText.AutoSize = true;
+            labelCoinText.Location = new Point(143, 9);
+            labelCoinText.Name = "labelCoinText";
+            labelCoinText.Size = new Size(49, 15);
+            labelCoinText.TabIndex = 15;
+            labelCoinText.Text = "Coin - 0";
+            // 
+            // labelHP
+            // 
+            labelHP.AutoSize = true;
+            labelHP.Location = new Point(75, 9);
+            labelHP.Name = "labelHP";
+            labelHP.Size = new Size(62, 15);
+            labelHP.TabIndex = 14;
+            labelHP.Text = "HP - 100%";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.BackColor = SystemColors.ControlDarkDark;
+            label13.Location = new Point(177, 180);
+            label13.Name = "label13";
+            label13.Size = new Size(19, 195);
+            label13.TabIndex = 13;
+            label13.Text = "    \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = SystemColors.ControlDarkDark;
+            label12.Location = new Point(177, 180);
+            label12.Name = "label12";
+            label12.Size = new Size(103, 15);
+            label12.TabIndex = 12;
+            label12.Text = "                                \r\n";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = SystemColors.ControlDarkDark;
+            label11.Location = new Point(636, 255);
+            label11.Name = "label11";
+            label11.Size = new Size(94, 15);
+            label11.TabIndex = 11;
+            label11.Text = "                             ";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = SystemColors.ControlDarkDark;
+            label10.Location = new Point(711, 60);
+            label10.Name = "label10";
+            label10.Size = new Size(19, 195);
+            label10.TabIndex = 10;
+            label10.Text = "    \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = SystemColors.ControlDarkDark;
+            label9.Location = new Point(546, 60);
+            label9.Name = "label9";
+            label9.Size = new Size(184, 15);
+            label9.TabIndex = 9;
+            label9.Text = "                                                           ";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = SystemColors.ControlDarkDark;
+            label8.Location = new Point(536, 60);
+            label8.Name = "label8";
+            label8.Size = new Size(19, 195);
+            label8.TabIndex = 8;
+            label8.Text = "    \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = SystemColors.ControlDarkDark;
+            label7.Location = new Point(536, 255);
+            label7.Name = "label7";
+            label7.Size = new Size(19, 195);
+            label7.TabIndex = 7;
+            label7.Text = "    \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = SystemColors.ControlDarkDark;
+            label6.Location = new Point(451, 195);
+            label6.Name = "label6";
+            label6.Size = new Size(19, 195);
+            label6.TabIndex = 6;
+            label6.Text = "    \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.ControlDarkDark;
+            label5.Location = new Point(286, 180);
+            label5.Name = "label5";
+            label5.Size = new Size(184, 15);
+            label5.TabIndex = 5;
+            label5.Text = "                                                           ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.ControlDarkDark;
+            label4.Location = new Point(272, 255);
+            label4.Name = "label4";
+            label4.Size = new Size(19, 195);
+            label4.TabIndex = 4;
+            label4.Text = "    \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ControlDarkDark;
+            label3.Location = new Point(272, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(19, 195);
+            label3.TabIndex = 3;
+            label3.Text = "    \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Yellow;
+            label2.Location = new Point(12, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 0;
+            label2.Text = "НАЧАЛО";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ControlDarkDark;
+            label1.Location = new Point(0, 180);
+            label1.Name = "label1";
+            label1.Size = new Size(103, 15);
+            label1.TabIndex = 1;
+            label1.Text = "                                \r\n";
+            // 
+            // hero
+            // 
+            hero.AutoSize = true;
+            hero.BackColor = Color.Red;
+            hero.Location = new Point(104, 83);
+            hero.Name = "hero";
+            hero.Size = new Size(31, 15);
+            hero.TabIndex = 2;
+            hero.Text = "hero";
+            hero.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label hero;
+        private Label label1;
+        private Label label2;
+        private Label label11;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label13;
+        private Label label12;
+        private Label labelHP;
+        private Label labelCoinText;
+        private Label labelCoin1;
+        private Label label16;
+        private Label label15;
+        private Label labelCoin2;
+        private Label Coin1;
+        private Label labelCoin5;
+        private Label labelCoin4;
+        private Label labelCoin3;
+        private Label labelCoin7;
+        private Label labelCoin6;
+        private Label poisonHP1;
+        private Label poisonHP2;
+    }
+}
---
+++ HWFormsmaze/Form1.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HWFormsmaze
+{
+    public partial class Form1 : Form
+    {
+        private Point startPosition;
+        private int healthPoints = 100;
+        private int collectedCoins = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            startPosition = hero.Location;
+            labelHP.Text = "HP: 100%";
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            int stepSize = 5;
+
+            if (keyData == Keys.Left)
+            {
+                hero.Left -= stepSize;
+            }
+            else if (keyData == Keys.Right)
+            {
+                hero.Left += stepSize;
+            }
+            else if (keyData == Keys.Up)
+            {
+                hero.Top -= stepSize;
+            }
+            else if (keyData == Keys.Down)
+            {
+                hero.Top += stepSize;
+            }
+
+            bool isCollision = false;
+            for (int i = 1; i <= 13; i++)
+            {
+                string labelWall = "label" + i;
+                Label labelW = Controls.Find(labelWall, true).FirstOrDefault() as Label;
+
+                if (labelW != null && hero.Bounds.IntersectsWith(labelW.Bounds))
+                {
+                    isCollision = true;
+                    break;
+                }
+            }
+            for (int i = 1; i <= 7; i++)
+            {
+                string labelCoin = "labelCoin" + i;
+                Label labelC = Controls.Find(labelCoin, true).FirstOrDefault() as Label;
+                if (labelC != null && hero.Bounds.IntersectsWith(labelC.Bounds))
+                {
+                    collectedCoins++;
+                    Controls.Remove(labelC);
+                    labelC.Dispose();
+                    break;
+                }
+            }
+            for (int i = 1; i <= 2; i++)
+            {
+                string labelHeal = "poisonHP" + i;
+                Label labelH = Controls.Find(labelHeal, true).FirstOrDefault() as Label;
+                if (labelH != null && hero.Bounds.IntersectsWith(labelH.Bounds))
+                {
+                    if (healthPoints >= 100)
+                    {
+                        MessageBox.Show("В вас максимально хп!");
+                    }
+                    else if (healthPoints == 95)
+                    {
+                        healthPoints += 5;
+                        
+                    }
+                    else
+                    {
+                        healthPoints += 10;
+                    }
+                    Controls.Remove(labelH);
+                    labelH.Dispose();
+                    break;
+                }
+            }
+
+            if (isCollision || IsOutOfBounds(hero))
+            {
+                healthPoints -= 5;
+                if (healthPoints <= 0)
+                {
+                    MessageBox.Show("Ви програли!");
+                    Close();
+                }
+                else
+                {
+                    hero.Location = startPosition;
+                }
+            }
+            labelHP.Text = $"HP: {healthPoints}%";
+            labelCoinText.Text = $"Coins: {collectedCoins}";
+
+            if (collectedCoins == 7)
+            {
+                MessageBox.Show("Вітаємо, ви перемогли!");
+                Close();
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private bool IsOutOfBounds(Label label)
+        {
+            return label.Left < 0 || label.Right > ClientSize.Width || label.Top < 0 || label.Bottom > ClientSize.Height;
+        }
+    }
+}
